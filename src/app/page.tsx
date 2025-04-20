ca37b30bdde36a28d4e6dc8aefebb058eb9569ef
Chafik Pro <kchikachechafik.box.net@gmail.com>
2025-04-20 20:18:46 +0000

    la button 'S'inscrire' dans la page d'acceuil apparaissent plus large que la button 'se connecter'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
           {/* Grid changes to 2 columns on medium screens and up */}
           <div className="flex flex-col items-center justify-center p-8 rounded-lg shadow-md bg-card">
             <h2 className="text-2xl font-semibold mb-4 text-foreground">
-              Colie est package
+              colie est package
             </h2>
             <p className="text-muted-foreground mb-6 text-center">
             Expédiez vos colis en un clin d’œil...
